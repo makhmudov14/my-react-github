65d8280f8c4f51f9114952d8883a4c9f848a3a07
mac <mac@macui-MacBookPro.local>
2023-12-30 13:46:37 +0900

    pushing project

---
+++ src/context/mockData.js/mockUser.js
@@ -0,0 +1,39 @@
+export default {
+  login: 'Asadbek Mkahmudov',
+  id: 42133389,
+  node_id: 'MDQ6VXNlcjQyMTMzMzg5',
+  avatar_url: 'https://images.unsplash.com/photo-1595617795501-9661aafda72a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/john-smilga',
+  html_url: 'https://github.com/john-smilga',
+  followers_url: 'https://api.github.com/users/john-smilga/followers',
+  following_url:
+    'https://api.github.com/users/john-smilga/following{/other_user}',
+  gists_url: 'https://api.github.com/users/john-smilga/gists{/gist_id}',
+  starred_url:
+    'https://api.github.com/users/john-smilga/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/john-smilga/subscriptions',
+  organizations_url: 'https://api.github.com/users/john-smilga/orgs',
+  repos_url: 'https://api.github.com/users/john-smilga/repos',
+  events_url: 'https://api.github.com/users/john-smilga/events{/privacy}',
+  received_events_url:
+    'https://api.github.com/users/john-smilga/received_events',
+  type: 'User',
+  site_admin: false,
+  name: 'Asadbek Makhmudov',
+  company: 'Samsung',
+  blog: 'www.makhmudov.com',
+  location: 'Seoul, South Korea',
+  email: null,
+  hireable: null,
+  bio: 'Full-stack developer',
+  twitter_username: 'makhmudov_page',
+  public_repos: 152,
+  public_gists: 0,
+  followers: 1495,
+  following: 0,
+  created_at: '2018-08-06T06:48:23Z',
+  updated_at: '2020-07-08T05:01:32Z',
+};
+
+//  https://avatars3.githubusercontent.com/u/42133389?v=4
